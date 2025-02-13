bee35615e478f51f539a69e0482304c209c3d932
Emerson Souza <166417132+EmersonDevLucas@users.noreply.github.com>
2025-02-13 19:47:12 -0300

    Update
    Foi realizado uma atualização e aplicado o MCV para que eu possa retomar o projeto utilizando microserviços com RESTful APIs.

--- agenda/script.js
+++
@@ -1,7 +0,0 @@
-let passarPróximo = document.getElementById('direita');
-let passarAnterior = document.getElementById('esquerda');
-let ano = document.getElementById('info-ano');
-let mes = document.getElementById('info-mes');
-let dia = document.getElementById('info-dia');
-let hora = document.getElementById('info-hora');
-
--- home/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('agendar').onclick = function () {
-    window.location.replace('https://emersondevlucas.github.io/app-barber-mobile/inicio/')
-}
-
-document.getElementById('localização').onclick = function () {
-    window.location.replace('https://www.google.com.br/maps')
-}
